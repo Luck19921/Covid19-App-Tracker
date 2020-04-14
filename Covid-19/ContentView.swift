63ba8883654eb6c051821a84e59f02f9e2eb980f
Albert_Cheng <albert_cheng@gamteks.com>
2020-04-14 10:37:18 +0800

    Initial Commit

---
+++ Covid-19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-19
+//
+//  Created by Albert Cheng on 2020/4/14.
+//  Copyright © 2020 Albert Cheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
